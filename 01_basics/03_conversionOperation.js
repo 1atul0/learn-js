b34bc854e7dd8121cf54dd4a09f4f16d45dedd5e
Atul  kumar <919atul@gmail.com>
2024-04-28 12:56:54 +0000

    conversion learned

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score="89" //give op:- number,89
+// let score="89adb" // give op:- number,NaN
+// let score=null; //o/p:- number,0
+// let score=undefined //op:- number, NaN
+let score=true // op:- number,1
+
+
+// console.log(typeof score);
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  "33"=> 33
+//  "33abc"=> NaN
+//  true=> 1; false=>0
+
+let isLoggedIn="";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//1=> true; 0=> false
+//""=> false
+//"atul"=> true
+
+let letNumber=34;
+let stringNumber=String(letNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
