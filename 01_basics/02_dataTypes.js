a2e2411be9e715e5eac9c8e56632bbc071c5ad81
Atul  kumar <919atul@gmail.com>
2024-04-28 12:37:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert not work in nodejs
+// alert("someting"); 
+
+let name="atul" //string
+let age=22 //Integer
+let isLoggedIn= false //boolean
+// let state;
+let state=null;
+
+//primitive data types
+//number=> 2to power 53
+//bigint
+//string
+//boolean
+//null=> standalone value
+//undefined => 
+//symbol=> unique
+
+// Non primitive data types 
+// Object
+
+console.log(typeof "atul"); //both method are valid for type checking
+console.log(typeof("atul"));
+console.log(typeof age);
+console.log(typeof null);//output object
+console.log(typeof undefined);// output undefined 
